ccb5ad2f15301f06d5269c4b208b9681bb20e382
AJ-Holzer <axel123.holzer@gmail.com>
2026-01-10 23:29:50 +0100

    Add documentation for GitHubRepositoryModel to clarify its purpose and fields.

--- api/src/api/models.py
+++ api/src/api/models.py
@@ -8,6 +8,8 @@ from pydantic import BaseModel
 
 
 class GitHubRepositoryModel(BaseModel):
+    """The GitHub repository containing the url, name, description and the commit count."""
+
     url: str
     name: str
     description: str
